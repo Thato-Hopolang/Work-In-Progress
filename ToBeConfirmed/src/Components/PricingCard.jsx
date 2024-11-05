b8bd64eead0220705e92f109a3866f45c8a52d49
Thato Hopolang Botsane <thbjr@outlook.com>
2024-11-05 22:55:39 +0200

    Creates PricingCard.jsx

---
+++ ToBeConfirmed/src/Components/PricingCard.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+
+const PricingCard = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+function itemIncluded({ item }) {
+    return (
+      <div>
+        <h1>{item}</h1>
+      </div>
+    )
+  
+}
+
+export default PricingCard
