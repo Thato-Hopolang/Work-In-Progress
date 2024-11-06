156c3692a7d4bb19519f8512db1b995acf8dac24
Thato Hopolang Botsane <thbjr@outlook.com>
2024-11-06 20:56:10 +0200

    Adds WhyUsCard.jsx adds content from design files used eslint-disable react/prop-types

---
+++ ToBeConfirmed/src/Components/WhyUsCard.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+const WhyUsCard = ({ heading, paragraph}) => {
+  return (
+    <div>
+        <img src="" alt="supporting image"/>
+        <div className="text-section">
+            <h3>{heading}</h3>
+            <p>{paragraph}</p>
+        </div>
+    </div>
+  )
+}
+
+export default WhyUsCard
