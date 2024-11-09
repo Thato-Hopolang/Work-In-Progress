aeca984983809e336d14899f196c6b9605b6afdf
Thato Hopolang Botsane <thbjr@outlook.com>
2024-11-09 23:57:17 +0200

    Adds Browers Router to main.jsx
    Adds styles for Navigation.jsx to App.css
    
    Adds Routing to App.jsx
    
    Completes Routing in Navigation.jsx

--- ToBeConfirmed/src/App.jsx
+++ ToBeConfirmed/src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css'
-import Navigation from './Components/Navigation/Navigation'
+import Navigation from './Components/Navigation/Navigation.jsx'
+import { Route, Routes } from 'react-router-dom'
+
+/* Pages */
+
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
 
 function App() {
 
+  console.log(window.location)
+
   return (
-    <>
-    <Navigation/>
-    </>
+    <div>
+      <Navigation/>
+      
+      <div className="routing">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+
+    </div>
   )
 }
 
--- ToBeConfirmed/src/Components/Navigation/Navigation.jsx
+++ ToBeConfirmed/src/Components/Navigation/Navigation.jsx
@@ -1,16 +1,32 @@
-// import { BrowserRoute, Route, Routes } from 'react-dom'
+/* eslint-disable react/prop-types */
+import { Link, useMatch, useResolvedPath } from 'react-dom'
 
 const Navigation = () => {
   return (
     <nav className='nav-bar'>
-      <a href="/">Website Logo</a>
+      <Link href="/">Website Logo</Link>
       <ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="">Contact</a></li>
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+        <CustomLink to="/contact">Contact</CustomLink>
       </ul>
     </nav>
   )
 }
 
 export default Navigation
+
+function CustomLink({ to, children, ...props }) {
+
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path : resolvedPath.pathname, end: true })
+
+
+  return (
+    <li className={isActive === to ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
