b97f4e6d6af7bb43bd0a9a38913d433b7bd0629c
Thato Hopolang Botsane <thbjr@outlook.com>
2024-11-12 22:06:58 +0200

    Updates RatingsCard.jsx:
    - Adding two function to mirror the redesign

--- ToBeConfirmed/src/Components/RatingsCard.jsx
+++ ToBeConfirmed/src/Components/RatingsCard.jsx
@@ -4,7 +4,7 @@ const RatingsCard = ({ name, occupation}) => {
   return (
     <div className="ratings-card">
         <div className="reviewer">
-            <img src="" alt="reviewer-profile" />
+            
             <div className="reviewer-identity">
                 <h4>{name}</h4>
                 <h4>{occupation}</h4>
@@ -15,3 +15,18 @@ const RatingsCard = ({ name, occupation}) => {
 }
 
 export default RatingsCard
+
+function AudioReview({ video}) {
+  return (
+    <video src={video} width="600" height="300" controls="controls" />
+  )
+}
+
+function TextReview() {
+  return (
+    <div>
+      <h1>Text Review</h1>
+      <p>This is a text review.</p>
+    </div>
+  )
+}
